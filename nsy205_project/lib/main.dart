4a75a8546d69f070c06f8898da76dea2dfdb5961
Antonio <antonio.semaan@lyze.com>
2021-02-21 22:53:08 +0200

    simplified main.dart

--- nsy205_project/lib/main.dart
+++ nsy205_project/lib/main.dart
@@ -10,24 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyHomePage(),
+      home: TablesPage(),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  
-  @override
-  Widget build(BuildContext context) {
-    return TablesPage();
   }
 }
